3fc8bce576e2510522eff06efa0cd572b3592c21
S F <sebastian.fariasb@usach.cl>
2020-11-28 08:35:33 -0300

    bug fix n1

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-const path = require('path')
+// const path = require('path')
 const itemRoutes = require('./api/routes/item')
 const userRoutes = require('./api/routes/user')
 const app = express()
 
-//Configura la static path 
-app.set(express.static(path.join(__dirname,'assets')))
+// //Configura la static path 
+// app.set(express.static(path.join(__dirname,'assets')))
 
 //Configura los headers
 app.use(express.urlencoded({extended: true}))
