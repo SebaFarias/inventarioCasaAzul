5ef3614fb7c18619782c37b59246b4969aeaac92
S F <sebastian.fariasb@usach.cl>
2020-11-28 08:54:15 -0300

    consoling log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 require('./database') 
+console.log('starting...')
 
 const server_port = process.env.YOUR_PORT || process.env.PORT || 8080
 const server_host = process.env.YOUR_HOST || '0.0.0.0';
